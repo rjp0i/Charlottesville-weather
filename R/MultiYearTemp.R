9feba4a83fbc2cd47523137cc6df64b8bc83d49d
Ricky Patterson <ricky@virginia.edu>
2025-05-06 21:03:48 -0400

    Commit

--- R/MultiYearTemp.R
+++ R/MultiYearTemp.R
@@ -112,9 +112,10 @@ plot_temp_panel <- function(target_year, var = "TMAX", show_x_axis = TRUE) {
       breaks = names(shape_map)
     ) +
     guides(
-      color = guide_legend(override.aes = list(size = 4, fill = color_map)),
-      shape = guide_legend(override.aes = list(size = 4, fill = color_map)),
+      color = guide_legend(override.aes = list(size = 4, fill = color_map, shape = shape_map)),
+      #shape = guide_legend(override.aes = list(size = 4, fill = color_map)),
       fill = "none"
+      shape = "none"
     ) +
     scale_y_continuous(
       breaks = seq(-10, 100, 10),
@@ -145,8 +146,8 @@ plot_temp_panel <- function(target_year, var = "TMAX", show_x_axis = TRUE) {
       plot.title = element_text(face = "bold", size = 14, color = ifelse(var == "TMAX", "#d1495b", "#3182bd")),
       axis.ticks = element_blank(),
       axis.text.x = if (show_x_axis) element_text() else element_blank(),
-      legend.position = c(0.5, 0.85),
-      legend.justification = c(0.5, 1),
+      legend.position.inside = c(0.5, 0.85),
+      legend.justification.inside = c(0.5, 1),
       legend.box.background = element_rect(fill = "white", colour = "gray50"),
       legend.box.margin = margin(6, 6, 6, 6)
     )
